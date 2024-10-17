294f75d16f2d60bf48eb99e57492a8260b2d142c
Matt <matt.zeitlinger@yahoo.com>
2024-10-17 18:51:34 -0400

    Fixed repeatString.js to pass all 7 tests.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let newWord = string;
+    for (let i = 1; i < num; i++) {
+        newWord += string;
+    }
+    if (num == 0) {
+        newWord = "";
+    }
+    if (num < 0) {
+        newWord = 'ERROR';
+    }
+    return newWord;
 };
 
 // Do not edit below this line
