353f8d56f18632f3ce79bfe8c973bed2a6223103
Matt <matt.zeitlinger@yahoo.com>
2024-10-17 22:02:54 -0400

    Edited removeFromArray.js to pass all the tests.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, element) {
+    let arrayLength = array.length;
+    for (let i = 1; i < arguments.length; i++) {
+        for (let j = 0; j < arrayLength; j++) {
+            if (arguments[i] === array[j]) {
+                array.splice(j, 1);
+                j--;
+            }
+        }
+    }
+    return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
